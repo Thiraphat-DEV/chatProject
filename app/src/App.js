e7265376a649e6a01190bba291be75f8616c8bbb
B0ATY <79009861+Thiraphat-DEV@users.noreply.github.com>
2022-02-04 10:19:31 +0700

    update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-import Chat from './components/Chat'
+import io from "socket.io-client";
+import "./App.css";
+import Chat from "./components/Chat";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+const socket = io.connect("http://localhost:3001");
 function App() {
+  const [username, setUsername] = useState(""); // ชื่อผู้ใช้
+  const [room, setRoom] = useState(""); // ห้องเเชท
+  const [show, setShow] = useState(false); // โชว์ห้องเเชท
+
+  const joinRoom = () => {
+    //เข้าร่วมห้อง
+    if (username !== "" && room !== "") {
+      //ถ้ามีการเข้ามาในห้อง
+      socket.emit("join_room", room); // ให้ทำการเข้าไปในห้องตามหมายเลขห้องที่ผู้ใช้กรอกเข้ามา
+      setShow(true); //เเสดงห้องเเชท
+    }
+  };
   return (
     <div className="App">
-      <Chat />
+      {!show ? (
+        <div className="joinChat"> 
+          <h3>Join Chat</h3>
+          <input
+            type="text"
+            placeholder="Enter UserName"
+            onChange={(e) => setUsername(e.target.value)} // กรอกชื่อผู้ใช้
+          />
+          <input
+            type="text"
+            placeholder="Enter RoomID"
+            onChange={(e) => setRoom(e.target.value)} // เข้าไปยังห้องที่ผู้ใช้กรอกเลข
+          />
+
+          <button onClick={joinRoom}>JOIN ROOM</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
     </div>
   );
 }
